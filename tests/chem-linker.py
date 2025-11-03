74ef5ddabcc84ca7a995653e63700921ff1cf622
Ramon Adàlia <adaliaramon@gmail.com>
2025-11-03 13:04:30 +0100

    Added another linker for testing

--- tests/chem-linker.py
+++ tests/chem-linker.py
@@ -8,9 +8,14 @@ from rdkit.Chem import Draw
 
 def main():
     monomer_library = load_monomer_library()
-    linkers = load_monomer_library(Path(__file__).parent / "data" / "puromycin.sdf")
+    linkers = load_monomer_library(Path(__file__).parent / "data" / "linkers.sdf")
     monomer_library["chem"] = linkers["chem"]
     helm = "PEPTIDE1{A.C}|RNA1{R(G)P.R(U)}|CHEM1{PURO}$PEPTIDE1,CHEM1,2:R2-1:R2|RNA1,CHEM1,1:R1-1:R1$$$V2.0"
+    molecule = Molecule(helm, monomer_df=monomer_library)
+    AllChem.Compute2DCoords(molecule.mol)
+    Draw.MolToImage(molecule.mol, size=(800, 800)).show()
+
+    helm = "PEPTIDE1{A.L}|RNA1{p.r(U)}|CHEM1{FAKE}$PEPTIDE1,CHEM1,2:R2-1:R1|RNA1,CHEM1,1:R1-1:R2$${}$V2.0"
     molecule = Molecule(helm, monomer_df=monomer_library)
     AllChem.Compute2DCoords(molecule.mol)
     Draw.MolToImage(molecule.mol, size=(800, 800)).show()
