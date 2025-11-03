a06ccb6936ef9faf7e7a5b9137f854c375026bcc
Ramon Adàlia <adaliaramon@gmail.com>
2025-11-03 11:19:08 +0100

    Corrected structure

--- tests/chem-linker.py
+++ tests/chem-linker.py
@@ -10,7 +10,7 @@ def main():
     monomer_library = load_monomer_library()
     linkers = load_monomer_library(Path(__file__).parent / "data" / "puromycin.sdf")
     monomer_library["chem"] = linkers["chem"]
-    helm = "PEPTIDE1{A.C}|RNA1{R(G)P.R(U)P}|CHEM1{PURO}$PEPTIDE1,CHEM1,2:2-1:1|RNA1,CHEM1,1:1-1:2$$$V2.0"
+    helm = "PEPTIDE1{A.C}|RNA1{R(G)P.R(U)}|CHEM1{PURO}$PEPTIDE1,CHEM1,2:2-1:2|RNA1,CHEM1,1:1-1:1$$$V2.0"
     molecule = Molecule(helm, monomer_df=monomer_library)
     AllChem.Compute2DCoords(molecule.mol)
     Draw.MolToImage(molecule.mol, size=(800, 800)).show()
