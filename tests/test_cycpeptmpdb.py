d89f3fc3b2dfed6ba44941c01be480c8716be481
Ramon Adàlia <adaliaramon@gmail.com>
2025-11-28 12:59:23 +0100

    Made test compatible with pyPept

--- tests/test_cycpeptmpdb.py
+++ tests/test_cycpeptmpdb.py
@@ -6,23 +6,46 @@ from helmkit import Molecule
 from rdkit import Chem
 from tqdm import tqdm
 
+USE_PYPEPT = False
+
+if USE_PYPEPT:
+    from pyPept.converter import Converter
+    from pyPept.molecule import Molecule
+    from pyPept.sequence import Sequence
+else:
+    from helmkit import Molecule
+
 
 def test():
     data_dir = Path(__file__).parent / "data"
     df = pl.read_csv(data_dir / "peptides.csv")
-    monomer_db = load_monomer_library(data_dir / "monomers.sdf")
+
+    if USE_PYPEPT:
+        monomer_lib_dir = str(data_dir.relative_to(Path.cwd()))
+        monomer_lib = "monomers.sdf"
+    else:
+        monomer_db = load_monomer_library(data_dir / "monomers.sdf")
+
+    errors = []
     for row in tqdm(df.iter_rows(named=True), total=df.height):
         helm = row["HELM"]
         smiles = row["SMILES"]
         try:
-            m = Molecule(helm, monomer_db)
-        except:
-            print(row)
+            if USE_PYPEPT:
+                converter = Converter(helm=helm)
+                sequence = Sequence(converter.get_biln(), monomer_lib_dir, monomer_lib)
+                m = Molecule(sequence)
+            else:
+                m = Molecule(helm, monomer_db)
+            inchi1 = Chem.MolToInchi(m.mol)
+            other = Chem.MolFromSmiles(smiles)
+            inchi2 = Chem.MolToInchi(other)
+            assert inchi1 == inchi2
+        except (Exception, SystemExit):
+            errors.append(helm)
             raise
-        inchi1 = Chem.MolToInchi(m.mol)
-        other = Chem.MolFromSmiles(smiles)
-        inchi2 = Chem.MolToInchi(other)
-        assert inchi1 == inchi2
+    print(f"Success rate: {(df.height - len(errors)) / df.height * 100:.2f}%")
+    print(f"Errors: {errors}")
 
 
 if __name__ == "__main__":
